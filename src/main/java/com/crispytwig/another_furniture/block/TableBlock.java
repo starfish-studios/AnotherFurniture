0d616c0aecb21066aafbf2977eaa9f9d9eb8985f
Luke Hartinger <lukehartinger@outlook.com>
2022-04-05 11:12:21 -0500

    [+~] Edited models/textures, added Table.

---
+++ src/main/java/com/crispytwig/another_furniture/block/TableBlock.java
@@ -0,0 +1,45 @@
+package com.crispytwig.another_furniture.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class TableBlock extends BaseBlock {
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+    protected static final VoxelShape TOP = Block.box(0.0D, 14.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+    protected static final VoxelShape SHAPE_NORTH = Shapes.or(TOP, Block.box(14.0D, 0.0D, 14.0D, 16.0D, 14.0D, 16.0D));
+    protected static final VoxelShape SHAPE_EAST = Shapes.or(TOP,Block.box(0.0D, 0.0D, 14.0D, 2.0D, 14.0D, 16.0D));
+    protected static final VoxelShape SHAPE_SOUTH = Shapes.or(TOP,Block.box(0.0D, 0.0D, 0.0D, 2.0D, 14.0D, 2.0D));
+    protected static final VoxelShape SHAPE_WEST = Shapes.or(TOP,Block.box(14.0D, 0.0D, 0.0D, 16.0D, 14.0D, 2.0D));
+
+    public TableBlock(Properties properties) {
+        super(properties);
+    }
+
+    public VoxelShape getShape(BlockState state, BlockGetter blockGetter, BlockPos pos, CollisionContext ctx) {
+        return switch (state.getValue(FACING)) {
+            case EAST -> SHAPE_EAST;
+            case SOUTH -> SHAPE_SOUTH;
+            case WEST -> SHAPE_WEST;
+            default -> SHAPE_NORTH;
+        };
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext ctx) {
+        return this.defaultBlockState().setValue(FACING, ctx.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+}
