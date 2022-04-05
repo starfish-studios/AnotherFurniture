ccf42ad2f493355986bf872393a94b0913a2f19c
Luke Hartinger <lukehartinger@outlook.com>
2022-04-05 11:28:13 -0500

    [~] Fixed Table, Shelf.

--- src/main/java/com/crispytwig/another_furniture/block/TableBlock.java
+++ src/main/java/com/crispytwig/another_furniture/block/TableBlock.java
@@ -15,22 +15,18 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 public class TableBlock extends BaseBlock {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     protected static final VoxelShape TOP = Block.box(0.0D, 14.0D, 0.0D, 16.0D, 16.0D, 16.0D);
-    protected static final VoxelShape SHAPE_NORTH = Shapes.or(TOP, Block.box(14.0D, 0.0D, 14.0D, 16.0D, 14.0D, 16.0D));
-    protected static final VoxelShape SHAPE_EAST = Shapes.or(TOP,Block.box(0.0D, 0.0D, 14.0D, 2.0D, 14.0D, 16.0D));
-    protected static final VoxelShape SHAPE_SOUTH = Shapes.or(TOP,Block.box(0.0D, 0.0D, 0.0D, 2.0D, 14.0D, 2.0D));
-    protected static final VoxelShape SHAPE_WEST = Shapes.or(TOP,Block.box(14.0D, 0.0D, 0.0D, 16.0D, 14.0D, 2.0D));
+    protected static final VoxelShape SHAPE_NORTH = Block.box(14.0D, 0.0D, 14.0D, 16.0D, 14.0D, 16.0D);
+    protected static final VoxelShape SHAPE_EAST = Block.box(0.0D, 0.0D, 14.0D, 2.0D, 14.0D, 16.0D);
+    protected static final VoxelShape SHAPE_SOUTH = Block.box(0.0D, 0.0D, 0.0D, 2.0D, 14.0D, 2.0D);
+    protected static final VoxelShape SHAPE_WEST = Block.box(14.0D, 0.0D, 0.0D, 16.0D, 14.0D, 2.0D);
+    protected static final VoxelShape SHAPE = Shapes.or(TOP, SHAPE_NORTH, SHAPE_EAST, SHAPE_SOUTH, SHAPE_WEST);
 
     public TableBlock(Properties properties) {
         super(properties);
     }
 
     public VoxelShape getShape(BlockState state, BlockGetter blockGetter, BlockPos pos, CollisionContext ctx) {
-        return switch (state.getValue(FACING)) {
-            case EAST -> SHAPE_EAST;
-            case SOUTH -> SHAPE_SOUTH;
-            case WEST -> SHAPE_WEST;
-            default -> SHAPE_NORTH;
-        };
+        return SHAPE;
     }
 
     @Override
