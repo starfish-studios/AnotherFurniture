f52f79d3be87b701d11cf83da100ddfc65c0f846
evanbones <bownessevan@gmail.com>
2022-05-21 22:54:57 -0700

    Add Biome Makeover woodtype compat

--- src/main/java/com/crispytwig/another_furniture/util/CompatUtil.java
+++ src/main/java/com/crispytwig/another_furniture/util/CompatUtil.java
@@ -2,6 +2,7 @@ package com.crispytwig.another_furniture.util;
 
 public class CompatUtil {
     public static final String BIOMESOPLENTY_ID = "biomesoplenty";
+    public static final String BIOMEMAKEOVER_ID = "biomemakeover";
     public static final String ECOLOGICS_ID = "ecologics";
     public static final String ENHANCED_MUSHROOMS_ID = "enhanced_mushrooms";
     public static final String QUARK_ID = "quark";
