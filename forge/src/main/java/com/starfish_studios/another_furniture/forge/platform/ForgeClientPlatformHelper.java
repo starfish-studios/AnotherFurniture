4c841046649d414daa51d6a7f8252e2687f7df97
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-06-05 01:59:02 -0400

    Add entity renderer registration helpers

--- forge/src/main/java/com/starfish_studios/another_furniture/forge/platform/ForgeClientPlatformHelper.java
+++ forge/src/main/java/com/starfish_studios/another_furniture/forge/platform/ForgeClientPlatformHelper.java
@@ -5,6 +5,10 @@ import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -15,6 +19,11 @@ public class ForgeClientPlatformHelper implements ClientPlatformHelper {
     @Override
     public void setRenderLayer(Supplier<Block> block, RenderType type) {
         ItemBlockRenderTypes.setRenderLayer(block.get(), type);
+    }
+
+    @Override
+    public <T extends Entity> void registerEntityRenderers(Supplier<EntityType<T>> type, EntityRendererProvider<T> renderProvider) {
+        EntityRenderers.register(type.get(), renderProvider);
     }
 
     @Override
