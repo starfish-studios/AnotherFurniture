be83c0ff4003f9d90bbc8aac0556eb7096813c3a
Synthestra (Jace) <blearsjace@gmail.com>
2022-06-18 11:35:09 +0100

    split tags into item + block tag registries

--- common/src/main/java/com/starfish_studios/another_furniture/registry/AFBlockTags.java
+++ common/src/main/java/com/starfish_studios/another_furniture/registry/AFBlockTags.java
@@ -7,21 +7,21 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
-public class AFTags {
+public class AFBlockTags {
+
+    public static final TagKey<Block> CHAIRS = blockTag("chairs");
+    public static final TagKey<Block> CURTAINS = blockTag("curtains");
+    public static final TagKey<Block> PLANTER_BOXES = blockTag("planter_boxes");
+    public static final TagKey<Block> SHELVES = blockTag("shelves");
+    public static final TagKey<Block> SHUTTERS = blockTag("shutters");
+    public static final TagKey<Block> STOOLS = blockTag("stools");
+    public static final TagKey<Block> TABLES = blockTag("tables");
 
     public static final TagKey<Block> CHAIRS_TUCKABLE_UNDER = blockTag("chairs_tuckable_under");
     public static final TagKey<Block> NO_SEAT_COLLISION_CHECK = blockTag("no_seat_collision_check");
     public static final TagKey<Block> HAMMER_INVALID = blockTag("hammer_invalid");
-    public static final TagKey<Block> CURTAINS = blockTag("curtains");
-
-    public static final TagKey<Item> PLANTER_BOX_PLACEABLES = itemTag("planter_box_placeables");
-    public static final TagKey<Item> PLANTER_BOX_BANNED = itemTag("planter_box_banned");
 
     private static TagKey<Block> blockTag(String name) {
         return TagKey.create(Registry.BLOCK_REGISTRY, new ResourceLocation(AnotherFurniture.MOD_ID, name));
-    }
-
-    private static TagKey<Item> itemTag(String name) {
-        return TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(AnotherFurniture.MOD_ID, name));
     }
 }
---
+++ common/src/main/java/com/starfish_studios/another_furniture/registry/AFItemTags.java
@@ -0,0 +1,26 @@
+package com.starfish_studios.another_furniture.registry;
+
+import com.starfish_studios.another_furniture.AnotherFurniture;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class AFItemTags {
+
+    public static final TagKey<Item> CHAIRS = itemTag("chairs");
+    public static final TagKey<Item> CURTAINS = itemTag("curtains");
+    public static final TagKey<Item> PLANTER_BOXES = itemTag("planter_boxes");
+    public static final TagKey<Item> SHELVES = itemTag("shelves");
+    public static final TagKey<Item> SHUTTERS = itemTag("shutters");
+    public static final TagKey<Item> STOOLS = itemTag("stools");
+    public static final TagKey<Item> TABLES = itemTag("tables");
+
+    public static final TagKey<Item> PLANTER_BOX_PLACEABLES = itemTag("planter_box_placeables");
+    public static final TagKey<Item> PLANTER_BOX_BANNED = itemTag("planter_box_banned");
+
+    private static TagKey<Item> itemTag(String name) {
+        return TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(AnotherFurniture.MOD_ID, name));
+    }
+}
