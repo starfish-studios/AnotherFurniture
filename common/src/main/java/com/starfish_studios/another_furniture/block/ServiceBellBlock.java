86e64f128348947c2d832689d126fa1a70382433
Synthestra (Jace) <blearsjace@gmail.com>
2022-09-01 15:12:44 +0100

    service bell finally got redstone power

--- common/src/main/java/com/starfish_studios/another_furniture/block/ServiceBellBlock.java
+++ common/src/main/java/com/starfish_studios/another_furniture/block/ServiceBellBlock.java
@@ -4,7 +4,10 @@ import com.starfish_studios.another_furniture.block.entity.ServiceBellBlockEntit
 import com.starfish_studios.another_furniture.registry.AFBlockEntityTypes;
 import com.starfish_studios.another_furniture.registry.AFSoundEvents;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -22,6 +25,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
@@ -33,10 +37,11 @@ public class ServiceBellBlock extends BaseEntityBlock implements SimpleWaterlogg
     protected static final VoxelShape SHAPE = Block.box(4.0D, 0.0D, 4.0D, 12.0D, 7.0D, 12.0D);
 
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+    public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
 
     public ServiceBellBlock(Properties properties) {
         super(properties);
-        registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, false));
+        registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, false).setValue(POWERED, false));
     }
 
     public VoxelShape getShape(BlockState state, BlockGetter blockGetter, BlockPos pos, CollisionContext context) {
@@ -52,12 +57,61 @@ public class ServiceBellBlock extends BaseEntityBlock implements SimpleWaterlogg
 
     @Override
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
-        BlockEntity blockentity = pLevel.getBlockEntity(pPos);
-        if (blockentity instanceof ServiceBellBlockEntity servicebellblockentity) {
-            servicebellblockentity.onHit();
+        if (pState.getValue(POWERED)) {
+            return InteractionResult.CONSUME;
+        } else {
+            this.press(pState, pLevel, pPos);
+            pLevel.gameEvent(pPlayer, GameEvent.BLOCK_ACTIVATE, pPos);
+
+            BlockEntity blockentity = pLevel.getBlockEntity(pPos);
+            if (blockentity instanceof ServiceBellBlockEntity servicebellblockentity) {
+                servicebellblockentity.onHit();
+            }
+            pLevel.playSound(null, pPos, AFSoundEvents.SERVICE_BELL.get(), SoundSource.BLOCKS, 1.0F, 1.0F);
+
+            return InteractionResult.sidedSuccess(pLevel.isClientSide);
         }
-        pLevel.playSound(null, pPos, AFSoundEvents.SERVICE_BELL.get(), SoundSource.BLOCKS, 1.0F, 1.0F);
-        return InteractionResult.SUCCESS;
+    }
+
+    public void press(BlockState state, Level level, BlockPos pos) {
+        level.setBlock(pos, state.setValue(POWERED, true), 3);
+        this.updateNeighbours(state, level, pos);
+        level.scheduleTick(pos, this, 2);
+    }
+
+    public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        if (state.getValue(POWERED)) {
+            level.setBlock(pos, state.setValue(POWERED, false), 3);
+            this.updateNeighbours(state, level, pos);
+            level.gameEvent(null, GameEvent.BLOCK_DEACTIVATE, pos);
+        }
+    }
+
+    public void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean isMoving) {
+        if (!isMoving && !state.is(newState.getBlock())) {
+            if (state.getValue(POWERED)) {
+                this.updateNeighbours(state, level, pos);
+            }
+
+            super.onRemove(state, level, pos, newState, isMoving);
+        }
+    }
+
+    public int getSignal(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return state.getValue(POWERED) ? 15 : 0;
+    }
+
+    public int getDirectSignal(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return state.getValue(POWERED) ? 15 : 0;
+    }
+
+    public boolean isSignalSource(BlockState state) {
+        return true;
+    }
+
+    private void updateNeighbours(BlockState state, Level level, BlockPos pos) {
+        level.updateNeighborsAt(pos, this);
+        level.updateNeighborsAt(pos.below(), this);
     }
 
     @Override
@@ -67,7 +121,7 @@ public class ServiceBellBlock extends BaseEntityBlock implements SimpleWaterlogg
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(WATERLOGGED);
+        pBuilder.add(WATERLOGGED).add(POWERED);
     }
 
     @Override
