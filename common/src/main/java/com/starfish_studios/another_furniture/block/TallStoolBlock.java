939e7db0eb0fcb28e75eebf4b59dc4c0c82d90ea
Synthestra (Jace) <blearsjace@gmail.com>
2022-09-09 23:30:06 +0100

    missed files + removed tools/ input +output dirs

--- common/src/main/java/com/starfish_studios/another_furniture/block/TallStoolBlock.java
+++ common/src/main/java/com/starfish_studios/another_furniture/block/TallStoolBlock.java
@@ -26,15 +26,12 @@ import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
-public class TallStoolBlock extends SeatBlock implements SimpleWaterloggedBlock {
-    protected static final VoxelShape AABB_BOTTOM = Block.box(3.0D, 0.0D, 3.0D, 13.0D, 20.0D, 13.0D);
-    protected static final VoxelShape AABB_TOP = Block.box(3.0D, -16.0D, 3.0D, 13.0D, 4.0D, 13.0D);
-
-    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-    public static final BooleanProperty TOP = BooleanProperty.create("top");
+public class TallStoolBlock extends StoolBlock {
+    protected static final VoxelShape AABB = Shapes.or(Block.box(4.0D, 0.0D, 4.0D, 12.0D, 12.0D, 12.0D), Block.box(2.0D, 12.0D, 2.0D, 14.0D, 16.0D, 14.0D));
 
     public TallStoolBlock(Properties properties) {
         super(properties);
@@ -43,87 +40,11 @@ public class TallStoolBlock extends SeatBlock implements SimpleWaterloggedBlock 
 
     @Override
     public float seatHeight() {
-        return 0.0F;
-    }
-
-    @Override
-    public boolean isSittable(BlockState state) {
-        return state.getValue(TOP);
-    }
-
-    @Override
-    public BlockPos dismountLocationOffset(BlockPos pos) {
-        return pos.below();
+        return 0.7F;
     }
 
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
-        return pState.getValue(TOP) ? AABB_TOP : AABB_BOTTOM;
+        return AABB;
     }
-
-    @Nullable
-    @Override
-    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
-        boolean waterlogged = pContext.getLevel().getFluidState(pContext.getClickedPos()).getType() == Fluids.WATER;
-        return pContext.getLevel().getBlockState(pContext.getClickedPos().above()).canBeReplaced(pContext) ? this.defaultBlockState().setValue(WATERLOGGED, waterlogged).setValue(TOP, false) : null;
-    }
-
-    @Override
-    public void setPlacedBy(Level level, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
-        super.setPlacedBy(level, pos, state, placer, stack);
-        if (!level.isClientSide) {
-            boolean waterlogged = level.getFluidState(pos).getType() == Fluids.WATER;
-            BlockPos blockPos = pos.above();
-            level.setBlock(blockPos, state.setValue(WATERLOGGED, waterlogged).setValue(TOP, true), 3);
-            level.blockUpdated(pos, Blocks.AIR);
-            state.updateNeighbourShapes(level, pos, 3);
-        }
-
-    }
-
-    @Override
-    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
-        BlockState top = pState;
-        BlockPos pos = pPos;
-
-        if(!pState.getValue(TOP)) {
-            BlockState above = pLevel.getBlockState(pPos.above());
-            if (above.getBlock() instanceof TallStoolBlock && above.getValue(TOP)) {
-                top = above;
-                pos = pPos.above();
-            }
-
-        }
-        return super.use(top, pLevel, pos, pPlayer, pHand, pHit);
-    }
-
-    @Override
-    public FluidState getFluidState(BlockState pState) {
-        return pState.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(pState);
-    }
-
-    @Override
-    public BlockState updateShape(BlockState pState, Direction pDirection, BlockState pNeighborState, LevelAccessor pLevel, BlockPos pCurrentPos, BlockPos pNeighborPos) {
-        if (pState.getValue(WATERLOGGED)) {
-            pLevel.scheduleTick(pCurrentPos, Fluids.WATER, Fluids.WATER.getTickDelay(pLevel));
-        }
-        Direction dir = pState.getValue(TOP) ? Direction.DOWN : Direction.UP;
-        if (pDirection == dir) {
-            if (!(pNeighborState.is(this) && pState.getValue(TOP) != pNeighborState.getValue(TOP))) {
-                return Blocks.AIR.defaultBlockState();
-            }
-        }
-        return super.updateShape(pState, pDirection, pNeighborState, pLevel, pCurrentPos, pNeighborPos);
-    }
-
-    @Override
-    public boolean isPathfindable(BlockState state, BlockGetter level, BlockPos pos, PathComputationType type) {
-        return false;
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(WATERLOGGED).add(TOP);
-    }
-
 }
--- tools/main.py
+++ tools/main.py
@@ -24,12 +24,12 @@ project_dir = Path(os.getcwd()).parent.absolute()
 namespace = load_properties(f"{project_dir}\\gradle.properties")["archives_base_name"]
 
 def main():
-    sided_helper.ensure_outputs_exist(f"{project_dir}\\forge\\src\\main\\resources\\assets")
-    sided_helper.convertForgeRenderTypes(f"{project_dir}\\forge\\src\\main\\resources\\assets\\{namespace}\\models\\block\\template")
-    texture_gen.generate_wood_types(
-        plank_path = "C:\\Users\\jacec\\Desktop\\planks",
-        template_path = "C:\\Users\\jacec\\Desktop\\another_furniture\\common\\src\\main\\resources\\assets\\another_furniture\\textures\\block",
-        banned_variants = ["chair", "shutter"]) # stops overriding textures that have custom patterns for each variant
+    #sided_helper.ensure_outputs_exist(f"{project_dir}\\forge\\src\\main\\resources\\assets")
+    #sided_helper.convertForgeRenderTypes(f"{project_dir}\\forge\\src\\main\\resources\\assets\\{namespace}\\models\\block\\template")
+    #texture_gen.generate_wood_types(
+    #    plank_path = "C:\\Users\\jacec\\Desktop\\planks",
+    #    template_path = "C:\\Users\\jacec\\Desktop\\another_furniture\\common\\src\\main\\resources\\assets\\another_furniture\\textures\\block",
+    #    banned_variants = ["chair", "shutter"]) # stops overriding textures that have custom patterns for each variant
     
 
     
