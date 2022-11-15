c496fa98f227581a017fad4964c02743cb2da965
Synthestra (Jace) <blearsjace@gmail.com>
2022-11-15 16:08:18 +0000

    awning fixed
    womp womp
    womp womp

--- common/src/main/java/com/starfish_studios/another_furniture/block/AwningBlock.java
+++ common/src/main/java/com/starfish_studios/another_furniture/block/AwningBlock.java
@@ -2,25 +2,26 @@ package com.starfish_studios.another_furniture.block;
 
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.SlabType;
-import net.minecraft.world.level.material.FluidState;
-import net.minecraft.world.level.material.Fluids;
 
 import javax.annotation.Nullable;
 
 public class AwningBlock extends SlabBlock {
 
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
     public AwningBlock(Properties properties) {
         super(properties);
         registerDefaultState(this.stateDefinition.any()
-                .setValue(BlockStateProperties.HORIZONTAL_FACING, Direction.NORTH)
-                .setValue(BlockStateProperties.WATERLOGGED, false)
-                .setValue(BlockStateProperties.SLAB_TYPE, SlabType.BOTTOM));
+                .setValue(WATERLOGGED, false)
+                .setValue(TYPE, SlabType.BOTTOM)
+                .setValue(FACING, Direction.NORTH));
     }
 
     @Override
@@ -28,26 +29,15 @@ public class AwningBlock extends SlabBlock {
             return false;
     }
 
-
-
-    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
-    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-
-    public FluidState getFluidState(BlockState state) {
-        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
-    }
-
     @Nullable
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        boolean waterlogged = context.getLevel().getFluidState(context.getClickedPos()).getType() == Fluids.WATER;
-        return this.defaultBlockState()
-                .setValue(FACING, context.getHorizontalDirection().getOpposite())
-                .setValue(WATERLOGGED, waterlogged);
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 
-    public BlockState getSlabBlockState(boolean isTop) {
-        return this.defaultBlockState().setValue(BlockStateProperties.SLAB_TYPE, isTop ? SlabType.TOP : SlabType.BOTTOM);
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(TYPE, FACING, WATERLOGGED);
     }
 
 }
