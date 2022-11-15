2858d258f70272da11e3aee6b1d8068fbb658440
crispytwig <lukehartinger@outlook.com>
2022-11-15 14:24:38 -0600

    [+] Added Awning tops and blockstates. Made Awnings bouncy.

--- common/src/main/java/com/starfish_studios/another_furniture/block/AwningBlock.java
+++ common/src/main/java/com/starfish_studios/another_furniture/block/AwningBlock.java
@@ -1,7 +1,13 @@
 package com.starfish_studios.another_furniture.block;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BedBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.state.BlockState;
@@ -9,6 +15,9 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.SlabType;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 import javax.annotation.Nullable;
 
@@ -25,6 +34,17 @@ public class AwningBlock extends SlabBlock {
     }
 
     @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        SlabType slabType = (SlabType)state.getValue(TYPE);
+        switch (slabType) {
+            case TOP:
+                return TOP_AABB;
+            default:
+                return BOTTOM_AABB;
+        }
+    }
+
+    @Override
     public boolean canBeReplaced(BlockState state, BlockPlaceContext useContext) {
             return false;
     }
@@ -32,12 +52,36 @@ public class AwningBlock extends SlabBlock {
     @Nullable
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+        return super.getStateForPlacement(context).setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
+
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(TYPE, FACING, WATERLOGGED);
+    }
+
+    // Bounce
+    public void fallOn(Level level, BlockState state, BlockPos pos, Entity entity, float fallDistance) {
+        super.fallOn(level, state, pos, entity, fallDistance * 0.5F);
+    }
+
+    public void updateEntityAfterFallOn(BlockGetter level, Entity entity) {
+        if (entity.isSuppressingBounce()) {
+            super.updateEntityAfterFallOn(level, entity);
+        } else {
+            this.bounceUp(entity);
+        }
+
+    }
+
+    private void bounceUp(Entity entity) {
+        Vec3 vec3 = entity.getDeltaMovement();
+        if (vec3.y < 0.0) {
+            double d = entity instanceof LivingEntity ? 1.0 : 0.8;
+            entity.setDeltaMovement(vec3.x, -vec3.y * 0.6600000262260437 * d, vec3.z);
+        }
+
     }
 
 }
