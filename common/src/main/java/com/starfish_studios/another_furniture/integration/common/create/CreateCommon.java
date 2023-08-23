0c7f9c9c06a67e9180b12b1612e9dcd09b2c9ffb
Synthestra <blearsjace@gmail.com>
2023-08-23 22:29:33 +0100

    bamboo and cherry chair models

--- common/src/main/java/com/starfish_studios/another_furniture/integration/common/create/CreateCommon.java
+++ common/src/main/java/com/starfish_studios/another_furniture/integration/common/create/CreateCommon.java
@@ -1,6 +1,5 @@
 package com.starfish_studios.another_furniture.integration.common.create;
 
-import com.starfish_studios.another_furniture.block.CurtainBlock;
 import com.starfish_studios.another_furniture.block.ShutterBlock;
 import com.starfish_studios.another_furniture.block.properties.VerticalConnectionType;
 import net.minecraft.core.Direction;
@@ -9,12 +8,10 @@ import net.minecraft.world.level.block.state.BlockState;
 public class CreateCommon {
     public static boolean canStickToContraption(BlockState state, Direction direction) {
         if (state.getBlock() instanceof ShutterBlock) {
-            if (state.getValue(ShutterBlock.TYPE) == VerticalConnectionType.MIDDLE && direction.getAxis().isVertical()) return true;
-            if (state.getValue(ShutterBlock.TYPE) == VerticalConnectionType.TOP && direction == Direction.DOWN) return true;
-            if (state.getValue(ShutterBlock.TYPE) == VerticalConnectionType.BOTTOM && direction == Direction.UP) return true;
-        }
-        if (state.getBlock() instanceof CurtainBlock) {
-            if (direction.getAxis().isVertical() && state.getValue(CurtainBlock.FACING_VERTICAL) == direction) return true;
+            VerticalConnectionType type = state.getValue(ShutterBlock.TYPE);
+            if (type == VerticalConnectionType.MIDDLE && direction.getAxis().isVertical()) return true;
+            if (type == VerticalConnectionType.TOP && direction == Direction.DOWN) return true;
+            if (type == VerticalConnectionType.BOTTOM && direction == Direction.UP) return true;
         }
         return false;
     }
