39b0a004d9f7bbd759935baf8e5bc3c30fa7f66c
Synthestra <blearsjace@gmail.com>
2025-10-14 10:45:45 +0100

    forge 3.0.3

--- forge/src/main/java/com/starfish_studios/another_furniture/integration/forge/create/SeatMovementBehavior.java
+++ forge/src/main/java/com/starfish_studios/another_furniture/integration/forge/create/SeatMovementBehavior.java
@@ -15,7 +15,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
-import net.createmod.catnip.utility.VecHelper;
+import net.createmod.catnip.math.VecHelper;
 
 public class SeatMovementBehavior implements MovementBehaviour {
 
